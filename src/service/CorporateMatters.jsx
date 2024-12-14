94a2c3d1b36628ea262199068d4ef05f1cb3d06e
yugaldhiman <yugaldhiman14@gmail.com>
2024-12-14 21:31:49 +0530

    corporat matters page add

--- src/service/CorporateMatters.jsx
+++ src/service/CorporateMatters.jsx
@@ -1,11 +1,51 @@
-import React from 'react'
+import React from "react";
+import image from "../assets/corporate.jpg";
+import Contact from "../pages/Contact";
+import "./CorporateMatters.css";
 
 const CorporateMatters = () => {
     return (
-        <div>
-            <h1>CorporateMatters</h1>
-        </div>
-    )
-}
+        <>
+            <div className="corporateMattersMAinBox">
+                <h1>Corporate Matters</h1>
+                <p>Home / Services / Corporate Matters</p>
+                <div className="corporateMattersMainBoxChilde_left">
+                    <img src={image} alt="image not found" />
+                    <p>
+                        We provides consultation services to clients on matters related to
+                        the company law by consulting the individuals and groups from the
+                        very beginning like procedures related to formation and fulfilling
+                        mandatory obligations during operation of the company. Our services
+                        include: -
+                    </p>
+                    <p>
+                        <span>*</span> Formation & Registration of Public /Private /OPC/
+                        NBFC Company
+                    </p>
+                    <p>
+                        <span>*</span> E- Filing of Forms & Documents with MCA
+                    </p>
+                    <p>
+                        <span>*</span> Compliance under Companies Act, 2013 Getting
+                    </p>
+                    <p>
+                        <span>*</span> The Company name/location changed
+                    </p>
+                    <p>
+                        <span>*</span> Appointment/removal/resignation of Directors in
+                        company Maintenance of statutory records prescribed under Companies
+                        Act, 2013 Merger and Acquisition of Companies
+                    </p>
+                    <p>
+                        <span>*</span> Corporate financing/PE Funding/IPO Listing
+                    </p>
+                </div>
+                <div className="corporateMattersMainBoxChilde_right">
+                    <Contact />
+                </div>
+            </div>
+        </>
+    );
+};
 
-export default CorporateMatters
+export default CorporateMatters;
