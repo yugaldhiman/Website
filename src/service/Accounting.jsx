54181ac1398820a39e9de8c4b86ac18331c9a2f1
yugaldhiman <yugaldhiman14@gmail.com>
2024-12-14 20:07:12 +0530

    add accounting page

--- src/service/Accounting.jsx
+++ src/service/Accounting.jsx
@@ -1,12 +1,34 @@
-import { useParams } from 'react-router-dom';
-
+import React from "react";
+import image from "../assets/accounting.jpg";
+import Contact from '../pages/Contact'
+import './Accounting.css'
 function Accounting() {
-    const { id } = useParams();
-    return <>
-        <h1>Accounting Service</h1>
-    </>;
+    return (
+        <>
+            <div className="accountingMAinBox">
+                <h1>Accounting</h1>
+                <p>Home / Services / Accounting</p>
+                <div className="accountingMainBoxChilde_left">
+                    <img src={image} alt="image not found" />
+                    <p>
+                        We provide cost-effective and qualitative Financial Accounting
+                        Services to Corporate, firms as well as individuals. We provide
+                        accurate and timely services full range of accounting, book writing
+                        services, quarterly updates and financial statement preparation
+                        services. Some of the services offered by us include:
+                    </p>
+                    <p> Book-Writing</p>
+                    <p>Payroll Reconciliation</p>
+                    <p>Checking Account Reconciliation</p>
+                    <p>Accounts Receivable/Payable Reconciliations</p>
+                    <p>Preparation of Management Accounts and Statutory Accounts.</p>
+                </div>
+                <div className="accountingMainBoxChilde_right">
+                    <Contact />
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default Accounting;
-
-
