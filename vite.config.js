d5d9b7143e03b71e2e019d3c513c16078f56078e
yugaldhiman <yugaldhiman14@gmail.com>
2024-12-16 11:56:47 +0530

    change vite config files

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+
+
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
+
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    extensions: ['.js', '.jsx'], // Ensure Vite resolves .jsx files
+  },
+}); 
