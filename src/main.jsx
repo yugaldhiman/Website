b31ce4830ea1b966061235578027ca44e7885cf4
yugaldhiman <yugaldhiman14@gmail.com>
2024-12-16 12:15:12 +0530

    rename  files

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App.jsx';
+import App from './App.js';
 import './index.css';
 import Header from './componant/Header.js';
 import Footer from './componant/Footer.js';
