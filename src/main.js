c947e1345b9acafb34563ac3e2c99e429c4c2060
yugaldhiman <yugaldhiman14@gmail.com>
2024-12-16 12:07:10 +0530

    rename  files

--- src/main.js
+++ src/main.js
@@ -3,8 +3,8 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './index.css';
-import Header from './componant/Header.jsx';
-import Footer from './componant/Footer.jsx';
+import Header from './componant/Header.js';
+import Footer from './componant/Footer.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
